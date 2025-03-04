3a925bd873761db8b0e13cb486224170e7f40f37
Airat Gilmanov <archi999999@gmail.com>
2025-03-04 20:24:18 +0300

    init

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'typeface-roboto';
+import './styles/index.scss'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/shared/assets/icons/fill/index.ts
@@ -0,0 +1,5 @@
+export  { default as TrashIcon } from './trash.svg?react'
+export { default as GridIcon } from './grid.svg?react'
+export { default as StackGridIcon } from './stack-grid.svg?react'
+export { default as ListIcon } from './list.svg?react'
+export { default as ArrowBackIcon } from './arrow-back.svg?react'
